81b3e313b11676ede5be6b4c54f69d5ad35ef035
Mees van Wel <mees.vanwel@hexa-it.nl>
2023-07-25 18:12:55 +0200

    ~

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./footer.module.scss";
-
-export const Footer = () => {
-  return (
-    <footer className={styles.container}>
-      <p>© Cindy Irmgard 2023</p>
-    </footer>
-  );
-};
